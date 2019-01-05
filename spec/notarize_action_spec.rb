b342208d5b539e526284b9d036198d43ecbc9bf6
Berk Çebi <berk@zeplin.io>
2019-01-05 14:48:08 -0800

    Remove test, adding a TODO

--- spec/notarize_action_spec.rb
+++ spec/notarize_action_spec.rb
@@ -1,9 +1,3 @@
 describe Fastlane::Actions::NotarizeAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with('Notarize plugin is working!')
-
-      Fastlane::Actions::NotarizeAction.run(nil)
-    end
-  end
+  # TODO: Add .app bundle fixtures to test.
 end
